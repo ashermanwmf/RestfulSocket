4865af46153ae903bde80511a68591f90b30bf11
ashermanwmf <shermanandrew50@gmail.com>
2017-04-16 18:26:06 -0700

    finalized webpack

--- src/RestfulSocket/webpack.config.js
+++ src/RestfulSocket/webpack.config.js
@@ -1,16 +1,19 @@
+const path = require('path');
 const webpack = require('webpack');
+const BUILD_DIR = path.resolve(__dirname, 'dist/');
+const APP_DIR = path.resolve(__dirname, 'lib/');
 
 const config = {
-  entry: ['babel-polyfill', './RestfulSocket.js'],
+  entry: ['babel-polyfill', `${APP_DIR}/RestfulSocket.js`],
   output: {
-    path: './dist/',
+    path: BUILD_DIR,
     filename: 'bundle.js'
   },
   module : {
     loaders : [
       {
         test : /\.js?/,
-        include : '../RestfulSocket',
+        include : APP_DIR,
         loader : 'babel-loader'
       }
     ]
